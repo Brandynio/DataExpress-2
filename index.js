4e7b211cc295a38d6c8ca102b5d597b0aee4f14f
Cameron <CLofton@student.neumont.edu>
2018-12-01 16:50:44 -0700

    Made questions

---
+++ index.js
@@ -0,0 +1,25 @@
+//Question 1:
+    //Which Neumont degree is the best?
+        //A. Web Dev
+        //B. Game Dev
+        //C. IS
+        //D. TM
+        //E. CS
+
+//Question 2:
+    //Which Pokemon generation is the best?
+        //A. Gen 1
+        //B. Gen 2
+        //C. Gen 3
+        //D. Gen 4
+        //E. Gen 5
+        //F. Gen 6
+        //G. Gen 7
+
+//Question 3:
+    //What system do you mainly play games on?
+        //A. PC
+        //B. PS4
+        //C. Xbox One
+        //D. Switch
+        //E. I don't play games
