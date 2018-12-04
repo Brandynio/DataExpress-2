98574a85fa7ac442fcffdf5e28ac60ae2fafa144
bluna5 <44988243+bluna5@users.noreply.github.com>
2018-12-04 15:03:05 -0700

    Mywork HopefullyWorks

---
+++ routes/routes.js
@@ -0,0 +1,100 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+});
+
+var accountSchema = mongoose.Schema({
+    Username: String,
+    Password: String,
+    Email: String,
+    Age: String,
+    Q1: String,
+    Q2: String,
+    Q3: String,
+  });
+
+  var userAccounts = mongoose.model('Account_Collection', accountSchema);
+
+
+  exports.index = function (req, res) {
+    userAccounts.find(function (err, account) {
+      if (err) return console.error(err);
+      res.render('index', {
+        title: 'Account List',
+        user: account
+      });
+    });
+  };
+
+  exports.create = function (req, res) {
+    res.render('Login', {
+        title: 'Login'
+    });
+  };
+
+  exports.createAccount = function (req, res) {
+    var account = new userAccounts({
+      username: req.body.Username,
+      password: req.body.Password,
+      email: req.body.Email,
+      age: req.body.Age,
+      q1: req.body.Q1,
+      q2: req.body.Q2,
+      q3: req.body.Q3
+    });
+    account.save(function (err, account) {
+      if (err) return console.error(err);
+      console.log(req.body.Username + ' added');
+    });
+    res.redirect('/');
+  };
+
+  exports.edit = function (req, res) {
+    userAccounts.findById(req.params.id, function (err, account) {
+      if (err) return console.error(err);
+      res.render('edit', {
+        title: 'Edit Account',
+        account: account
+      });
+    });
+  };
+
+  exports.editAccount = function (req, res) {
+    userAccounts.findById(req.params.id, function (err, account) {
+      if (err) return console.error(err);
+      account.username = req.body.Username;
+      account.password = req.body.Password;
+      account.email = req.body.Email;
+      account.age = req.body.Age;
+      account.answer1 = req.body.Q1;
+      account.answer2 = req.body.Q2;
+      account.answer3 = req.body.Q3;
+
+      accountSchema.save(function (err, account) {
+        if (err) return console.error(err);
+        console.log(req.body.Username + ' updated');
+      });
+    });
+    res.redirect('/');
+  };
+
+  exports.delete = function (req, res) {
+    accountSchema.findByIdAndRemove(req.params.id, function (err, account) {
+      if (err) return console.error(err);
+      res.redirect('/');
+    });
+  };
+
+  exports.details = function (req, res) {
+    accountSchema.findById(req.params.id, function (err, account) {
+      if (err) return console.error(err);
+      res.render('details', {
+        title: account.Username + "'s Details",
+        account: account
+      });
+    });
+  };
